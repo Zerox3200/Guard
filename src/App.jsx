af8333e51d5a44a8223bf3c40e7cd186dd4d95cd
Zerox3200 <ziad.ahmed01020338066@gmail.com>
2024-07-28 23:01:51 +0300

    Intro updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { lazy, Suspense } from 'react';
 import './App.css';
-import Loader from './components/Loader/Loader';
+import Home from './pages/Home/Home'
 
-const Home = lazy(() => import("./pages/Home/Home"))
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Home />
-    </Suspense>
+    <Home />
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from '../../components/Nav/Nav'
 import Intro from '../../components/Intro/Intro'
 import About from '../../components/About/About'
@@ -8,16 +8,34 @@ import Resiential from '../../Assets/IMG-20240728-WA0015.jpg'
 import Coprate from '../../Assets/IMG-20240728-WA0012.jpg'
 import Personal from '../../Assets/IMG-20240728-WA0009.jpg'
 import Footer from '../../components/Footer/Footer'
+import Loader from '../../components/Loader/Loader'
 
 export default function Home() {
-    return <div className='min-h-screen bg-gradient-to-tr from-black to-white'>
-        <Nav />
-        <Intro />
-        <About />
-        <Services loc={1} img={Events} Title={"Event Security"} />
-        <Services loc={2} img={Resiential} Title={"Residential Security"} />
-        <Services loc={1} img={Coprate} Title={"Corporate Security"} />
-        <Services loc={2} img={Personal} Title={"Personal Protection"} />
-        <Footer />
-    </div>
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setIsLoading(false);
+        }, 1200);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
+
+    return <>
+        {isLoading ? <Loader /> : <div className='min-h-screen bg-gradient-to-tr from-black to-white'>
+            <Nav />
+            <Intro />
+            <About />
+            <Services loc={1} img={Events} Title={"Event Security"} />
+            <Services loc={2} img={Resiential} Title={"Residential Security"} />
+            <Services loc={1} img={Coprate} Title={"Corporate Security"} />
+            <Services loc={2} img={Personal} Title={"Personal Protection"} />
+            <Footer />
+        </div>
+        }
+
+    </>
 }
